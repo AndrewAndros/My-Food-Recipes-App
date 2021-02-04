ca7ccc169dd7327169a75d549a8c4022f67ed6fb
Shubham <sshubhamsharma190@gmail.com>
2021-02-04 16:05:44 +0530

    squashed 2 commits

--- app/src/main/java/com/androsgames/foodrecipes/AppExecutors.kt
+++ app/src/main/java/com/androsgames/foodrecipes/AppExecutors.kt
@@ -7,6 +7,9 @@ import java.util.concurrent.Executors
 
 class AppExecutors {
 
+    //Created AC-15
+    //Commit number 2 on AC-15
+
     companion object {
         private var instance: AppExecutors? = null
         fun get(): AppExecutors {
