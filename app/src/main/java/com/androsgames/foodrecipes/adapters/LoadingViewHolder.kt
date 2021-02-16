4d95be7c293d3ec18cca33a8da4c01bf91a48bca
Rifa92 <tasfiyarifa57@gmail.com>
2021-02-16 21:22:22 +0600

    updated

--- app/src/main/java/com/androsgames/foodrecipes/adapters/LoadingViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package com.androsgames.foodrecipes.adapters
-
-import android.support.v7.widget.RecyclerView
-import android.view.View
-
-class LoadingViewHolder (itemView : View) : RecyclerView.ViewHolder(itemView)
-
-
--- app/src/main/java/com/androsgames/foodrecipes/adapters/SearchExhaustedViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package com.androsgames.foodrecipes.adapters
-
-import android.support.v7.widget.RecyclerView
-import android.view.View
-
-class SearchExhaustedViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-}
