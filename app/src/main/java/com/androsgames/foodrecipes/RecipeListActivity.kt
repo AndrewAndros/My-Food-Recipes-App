a2f22b8a75ded6a724a88ba6a3b93ef25e4f6496
lander-2113 <rsahu5498@gmail.com>
2021-04-08 13:23:32 +0530

    THis is just a test

--- app/src/main/java/com/androsgames/foodrecipes/RecipeListActivity.kt
+++ app/src/main/java/com/androsgames/foodrecipes/RecipeListActivity.kt
@@ -51,6 +51,7 @@ class RecipeListActivity : BaseActivity(), OnRecipeListener {
 
         val mToolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(mToolbar)
+        //This is a test comment
     }
 
     private fun displaySearchCategories() {
